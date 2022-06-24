5410c429de9258e95c6037d0de897d6fb7f16c69
atharav443 <atharavnaik3@gmail.com>
2022-06-24 22:31:56 +0530

    get route added 2

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const createError=require('http-errors')
 const dotenv=require('dotenv').config()
 const app=express();
 app.use(express.json())
-
+const PORT=process.env.PORT||3000
 //Initialize DB
 require('./initDB')()
 
+app.get('/', (req, res) => {
+  res.send(`Server running at ${PORT}`)
+})
 
 //for raagas route
-
 app.use('/raagas',RaagasRoutes);
 
-//for route which doesn't exist
 app.use((req,res)=>{
-    res.status(404).send("Error: Not Found")
+  res.status(404).send("Error: Not Found")
 })
-
-const PORT=process.env.PORT||3000
-app.get('/', (req, res) => {
-  res.send(`Server running at ${PORT}  `)
-})
-app.listen(PORT,()=>{
+app.listen(PORT,(req,res)=>{
   console.log(`Server Listening at ${PORT}`)
 })
