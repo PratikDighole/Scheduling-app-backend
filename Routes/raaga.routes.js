b5d9a91276e08f807865ba2903c6618ea9da0ad5
atharav443 <atharavnaik3@gmail.com>
2022-06-24 11:42:50 +0530

    added all routes get patch delete for single id

--- Routes/raaga.routes.js
+++ Routes/raaga.routes.js
@@ -1,9 +1,26 @@
 const express=require('express')
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    res.status(404).send('Getting all raagas')
+router.get('/raagas',(req,res)=>{
+    res.status(200).send('Getting all raagas')
 })
+
+router.post('/raagas',(req,res)=>{
+    res.status(200).send('adding  raagas')
+})
+
+router.get('/raagas/id',(req,res)=>{
+    res.status(200).send('Getting a single raag with id')
+})
+
+router.patch('/raagas/id',(req,res)=>{
+    res.status(200).send('Updating a single raag with id')
+})
+router.delete('/raagas/id',(req,res)=>{
+    res.status(200).send("Deleting a single raag with id")
+})
+
+
 
 
 module.exports=router;
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const app=express();
 
-const RaagasRoutes=require('./')
+const RaagasRoutes=require('./Routes/raaga.routes')
+
+app.use('/',RaagasRoutes);
 
 app.listen(3000,()=>{
   console.log("Server Listening at 3000..")
